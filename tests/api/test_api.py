2dddff9b49fbfec62fdde4f68a8772837f8f199e
Ihor Orlov <ihororlov70@gmail.com>
2024-01-29 19:31:16 +0200

    Design task 2

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,1 +1,11 @@
+import pytest
 
+
+@pytest.mark.check
+def test_name(user):
+    assert user.name == "Igor"
+
+
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == "Orlov"
